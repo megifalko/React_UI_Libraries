1f01c446370a239e7a6768c458ede6afb1a9a51e
Magdalena Falkowska <82031803+megifalko@users.noreply.github.com>
2022-04-12 20:07:40 +0200

    Add files via upload

---
+++ components/NextUIBlur.tsx
@@ -0,0 +1,61 @@
+import {Card, Col, Row, Button, Text} from "@nextui-org/react";
+import { useState } from "react";
+import NextUIModal from "./NextUIModal";
+
+interface propsType{
+	url: string;
+}
+
+const NextUIBlur = (props: propsType) => {
+    const [modalOpened, setModalOpened] = useState(false);
+	return (
+		<Card id={props.url} cover css={{w: "340px", height: "400px"}}>
+			<Card.Header css={{position: "absolute", zIndex: 1, top: 5}}>
+				<Col>
+					<Text size={14} weight="bold" transform="uppercase" color="#ffffffd2">
+						On Sale
+					</Text>
+					<Text h3 color="white">
+						Cactus
+					</Text>
+				</Col>
+			</Card.Header>
+			<Card.Body>
+				<Card.Image src={props.url} height={400} width="100%" alt="Card example background" />
+			</Card.Body>
+			<Card.Footer
+				blur
+				css={{
+					position: "absolute",
+					bgBlur: "#828282",
+					borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+					bottom: 0,
+					zIndex: 1,
+				}}>
+				<Row align="center">
+					<Col>
+						<Text color="#fff" size={14}>
+							With our cactus you will become a happy cactus owner.
+						</Text>
+					</Col>
+					<Col>
+						<Row justify="flex-end">
+                            <NextUIModal opened={modalOpened} onClose={() => setModalOpened(false)} />
+							<Button auto rounded color="secondary" onClick={() => setModalOpened(true)}>
+								<Text
+									css={{color: "$primary", letterSpacings: "widest"}}
+									size={12}
+									weight="bold"
+									transform="uppercase">
+									Buy now
+								</Text>
+							</Button>
+						</Row>
+					</Col>
+				</Row>
+			</Card.Footer>
+		</Card>
+	);
+};
+
+export default NextUIBlur;
---
+++ images/img.tsx
@@ -0,0 +1,13 @@
+import c1 from "./c1.jpg";
+import c2 from "./c2.jpg";
+import c3 from "./c3.jpg";
+import c4 from "./c4.jpg";
+import c5 from "./c5.jpg";
+import c6 from "./c6.jpg";
+import c7 from "./c7.jpg";
+import c8 from "./c8.jpg";
+import c9 from "./c9.jpg";
+
+const imgArray = [c1, c2, c3, c4, c5, c6, c7, c8, c9];
+
+export default imgArray;
